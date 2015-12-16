1aacf2deb5f2612dee0aca5ca9504a3df50ab42e
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2015-12-16 21:59:20 +0200

    added file saving in formatted view

--- psl/main.py
+++ psl/main.py
@@ -6,30 +6,33 @@ P3 = [(1, 5, 12), (2, 13, 9), (3, 17, 7), (6, 14, 18), (8, 15, 16), (11, 21, 20)
 P2 = [(2, 5), (3, 8), (4, 16), (6, 20), (7, 10), (9, 12), (14, 21), (15, 17)]
 results = []
 formatted_results = []
-for p7_el_idx, p7_el in enumerate(P7):
-    p7_el_deq = deque(p7_el)
-    for p7_el_deq_i in xrange(0, len(p7_el_deq)):
-        p7_rotated_el_deq = deque(p7_el_deq)
-        p7_rotated_el_deq.rotate(p7_el_deq_i)
-        for p3_el_idx, p3_el in enumerate(P3):
-            p3_el_deq = deque(p3_el)
-            for p3_el_deq_i in xrange(0, len(p3_el_deq)):
-                p3_rotated_el_deq = deque(p3_el_deq)
-                p3_rotated_el_deq.rotate(p3_el_deq_i)
-                for p2_el_idx, p2_el in enumerate(P2):
-                    p2_el_deq = deque(p2_el)
-                    for p2_el_deq_i in xrange(0, len(p2_el_deq)):
-                        p2_rotated_el_deq = deque(p2_el_deq)
-                        p2_rotated_el_deq.rotate(p2_el_deq_i)
-                        for p5_el_idx, p5_el in enumerate(P5):
-                            p5_el_deq = deque(p5_el)
-                            for p5_el_deq_i in xrange(0, len(p5_el_deq)):
-                                p5_rotated_el_deq = deque(p5_el_deq)
-                                p5_rotated_el_deq.rotate(p5_el_deq_i)
-                                result = [tuple(p7_rotated_el_deq), tuple(p3_rotated_el_deq), tuple(p2_rotated_el_deq), tuple(p5_rotated_el_deq)]
-                                results.append(result)
-                                iteration_text = u'P7_%d(%d)P3_%d(%d)P2_%d(%d)P5_%d(%d)' % (
-                                    p7_el_idx, p7_el_deq_i, p3_el_idx, p3_el_deq_i, p2_el_idx, p2_el_deq_i, p5_el_idx, p5_el_deq_i)
-                                formatted_result = {"iteration": iteration_text, "result": result}
-                                formatted_results.append(formatted_result)
+with open('./formatted_output_file.txt', 'w+') as formatted_output_file:
+    for p7_el_idx, p7_el in enumerate(P7):
+        p7_el_deq = deque(p7_el)
+        for p7_el_deq_i in xrange(0, len(p7_el_deq)):
+            p7_rotated_el_deq = deque(p7_el_deq)
+            p7_rotated_el_deq.rotate(p7_el_deq_i)
+            for p3_el_idx, p3_el in enumerate(P3):
+                p3_el_deq = deque(p3_el)
+                for p3_el_deq_i in xrange(0, len(p3_el_deq)):
+                    p3_rotated_el_deq = deque(p3_el_deq)
+                    p3_rotated_el_deq.rotate(p3_el_deq_i)
+                    for p2_el_idx, p2_el in enumerate(P2):
+                        p2_el_deq = deque(p2_el)
+                        for p2_el_deq_i in xrange(0, len(p2_el_deq)):
+                            p2_rotated_el_deq = deque(p2_el_deq)
+                            p2_rotated_el_deq.rotate(p2_el_deq_i)
+                            for p5_el_idx, p5_el in enumerate(P5):
+                                p5_el_deq = deque(p5_el)
+                                for p5_el_deq_i in xrange(0, len(p5_el_deq)):
+                                    p5_rotated_el_deq = deque(p5_el_deq)
+                                    p5_rotated_el_deq.rotate(p5_el_deq_i)
+                                    result = [tuple(p7_rotated_el_deq), tuple(p3_rotated_el_deq), tuple(p2_rotated_el_deq), tuple(p5_rotated_el_deq)]
+                                    results.append(result)
+                                    iteration_text = u'P7_%d(%d)P3_%d(%d)P2_%d(%d)P5_%d(%d)' % (
+                                        p7_el_idx, p7_el_deq_i, p3_el_idx, p3_el_deq_i, p2_el_idx, p2_el_deq_i, p5_el_idx, p5_el_deq_i)
+                                    formatted_result = {"iteration": iteration_text, "result": result}
+                                    formatted_output = iteration_text + " - " + str(result) + "\n"
+                                    formatted_output_file.writelines(formatted_output)
+                                    formatted_results.append(formatted_result)
 print(u'Elements processed=%d' % (len(results)))
