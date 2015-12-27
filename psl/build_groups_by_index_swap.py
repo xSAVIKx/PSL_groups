9711b2fd56b1df132201dcc209d15a2ed96e9fb2
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2015-12-27 21:23:25 +0200

    cleaned code

--- psl/build_groups_by_index_swap.py
+++ psl/build_groups_by_index_swap.py
@@ -5,6 +5,8 @@ from psl.PSL_constants import P5, P7, P3, P2
 
 results = []
 formatted_results = []
+unique_results = []
+unique_formatted_results = []
 first_group_element = deque(range(1, 22))
 
 
@@ -15,61 +17,41 @@ def makeSwap(swappable, original_index, indexes_to_swap):
     return swappable_copy
 
 
-swappable = copy.copy(first_group_element)
-print swappable
-p2_0 = copy.copy(P2[0])
-p2_0_rotated_deque = deque(p2_0)
-p2_0_rotated_deque.rotate(1)
-swapped_deque = makeSwap(swappable, p2_0, p2_0_rotated_deque)
-
-p5_0 = copy.copy(P5[0])
-p5_0_rotated_deque = deque(p5_0)
-p5_0_rotated_deque.rotate(1)
-swapped_deque = makeSwap(swapped_deque, p5_0, p5_0_rotated_deque)
-print swapped_deque
-
-with open('./build_groups_by_index_swap.txt', 'w+') as formatted_output_file:
-    for p7_el_idx, p7_el in enumerate(P7):
-        swappable = copy.copy(first_group_element)
-        p7_el_deq = deque(p7_el)
-        for p7_el_deq_i in xrange(0, len(p7_el_deq)):
-            p7_rotated_el_deq = deque(p7_el_deq)
-            p7_rotated_el_deq.rotate(p7_el_deq_i)
-            swapped_deque = makeSwap(swappable, p7_el_deq, p7_rotated_el_deq)
-            for p3_el_idx, p3_el in enumerate(P3):
-                p3_el_deq = deque(p3_el)
-                for p3_el_deq_i in xrange(0, len(p3_el_deq)):
-                    p3_rotated_el_deq = deque(p3_el_deq)
-                    p3_rotated_el_deq.rotate(p3_el_deq_i)
-                    swapped_deque = makeSwap(swapped_deque, p3_el_deq, p3_rotated_el_deq)
-                    for p2_el_idx, p2_el in enumerate(P2):
-                        p2_el_deq = deque(p2_el)
-                        for p2_el_deq_i in xrange(0, len(p2_el_deq)):
-                            p2_rotated_el_deq = deque(p2_el_deq)
-                            p2_rotated_el_deq.rotate(p2_el_deq_i)
-                            swapped_deque = makeSwap(swapped_deque, p2_el_deq, p2_rotated_el_deq)
-                            for p5_el_idx, p5_el in enumerate(P5):
-                                p5_el_deq = deque(p5_el)
-                                for p5_el_deq_i in xrange(0, len(p5_el_deq)):
-                                    p5_rotated_el_deq = deque(p5_el_deq)
-                                    p5_rotated_el_deq.rotate(p5_el_deq_i)
-                                    swapped_deque = makeSwap(swapped_deque, p5_el_deq, p5_rotated_el_deq)
-                                    result = list(swapped_deque)
-                                    results.append(result)
-                                    iteration_text = u'P7_%d(%d)P3_%d(%d)P2_%d(%d)P5_%d(%d)' % (
-                                        p7_el_idx, p7_el_deq_i, p3_el_idx, p3_el_deq_i, p2_el_idx, p2_el_deq_i, p5_el_idx, p5_el_deq_i)
-                                    formatted_result = {"iteration": iteration_text, "result": result}
-                                    formatted_output = iteration_text + " - " + str(result) + "\n"
-                                    formatted_output_file.writelines(formatted_output)
-                                    formatted_results.append(formatted_result)
+with open('./build_groups_by_index_swap_unique.txt', 'w+') as formatted_unique_output_file:
+    with open('./build_groups_by_index_swap.txt', 'w+') as formatted_output_file:
+        for p7_el_idx, p7_el in enumerate(P7):
+            swappable = copy.copy(first_group_element)
+            for p7_el_deq_i in xrange(0, len(p7_el)):
+                p7_rotated_el_deq = deque(p7_el)
+                p7_rotated_el_deq.rotate(p7_el_deq_i)
+                swapped_deque = makeSwap(swappable, p7_el, p7_rotated_el_deq)
+                for p3_el_idx, p3_el in enumerate(P3):
+                    for p3_el_deq_i in xrange(0, len(p3_el)):
+                        p3_rotated_el_deq = deque(p3_el)
+                        p3_rotated_el_deq.rotate(p3_el_deq_i)
+                        swapped_deque = makeSwap(swapped_deque, p3_el, p3_rotated_el_deq)
+                        for p2_el_idx, p2_el in enumerate(P2):
+                            for p2_el_deq_i in xrange(0, len(p2_el)):
+                                p2_rotated_el_deq = deque(p2_el)
+                                p2_rotated_el_deq.rotate(p2_el_deq_i)
+                                swapped_deque = makeSwap(swapped_deque, p2_el, p2_rotated_el_deq)
+                                for p5_el_idx, p5_el in enumerate(P5):
+                                    for p5_el_deq_i in xrange(0, len(p5_el)):
+                                        p5_rotated_el_deq = deque(p5_el)
+                                        p5_rotated_el_deq.rotate(p5_el_deq_i)
+                                        swapped_deque = makeSwap(swapped_deque, p5_el, p5_rotated_el_deq)
+                                        result = list(swapped_deque)
+                                        results.append(result)
+                                        iteration_text = u'P7_%d(%d)P3_%d(%d)P2_%d(%d)P5_%d(%d)' % (
+                                            p7_el_idx, p7_el_deq_i, p3_el_idx, p3_el_deq_i, p2_el_idx, p2_el_deq_i, p5_el_idx, p5_el_deq_i)
+                                        formatted_result = {"iteration": iteration_text, "result": result}
+                                        formatted_output = iteration_text + " - " + str(result) + "\n"
+                                        formatted_output_file.writelines(formatted_output)
+                                        formatted_results.append(formatted_result)
+                                        if not unique_results.__contains__(result):
+                                            unique_results.append(result)
+                                            unique_formatted_results.append(formatted_result)
+                                            formatted_unique_output_file.writelines(formatted_output)
 
 print(u'Elements processed=%d\n' % (len(results)))
-unique_results = []
-for result in results:
-    if not unique_results.__contains__(result):
-        unique_results.append(result)
-
 print(u'Unique results=%d\n') % (len(unique_results))
-with open('./build_groups_by_index_swap_unique.txt', 'w+') as formatted_output_file:
-    for result in unique_results:
-        formatted_output_file.writelines(str(result) + '\n')
